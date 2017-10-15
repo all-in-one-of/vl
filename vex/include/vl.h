7b9a6fcec0502b48c8d44139e689102c019d32aa
Alexey Smolenchuk <somesanctus@yandex.ru>
2017-10-15 14:16:21 +0300

    init firest working prototype

---
+++ vex/include/vl.h
@@ -0,0 +1,33 @@
+vector vl(int samples; float _maxdist, scatter, absorb)
+{
+
+//    float maxdist = 15;
+//    float scatter = 0.1;
+//    float absorb = 0;
+//    int samples = 16;
+    float maxdist = _maxdist;
+    float ext = scatter + absorb;
+    vector nI = normalize(I);
+
+    maxdist = min(maxdist, length(I));
+
+    vector tmp = 0;
+
+    for (int i=0; i<samples; i++)
+    {   
+        float r = rand(i+SID);
+        float dist = maxdist * r;
+        vector pos = nI * dist;
+        illuminance(pos, {0,0,0})
+        {
+            shadow(Cl, pos, L);
+            tmp += Cl*scatter*exp((-dist-length(L)) * scatter)*0.5;
+        }
+    }
+
+
+    return tmp*maxdist/samples;
+
+}
+
+
