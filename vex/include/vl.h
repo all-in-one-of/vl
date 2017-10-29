d741c0744e1f6e154781d6c45a34eaa171fdcd2b
Alexey Smolenchuk <somesanctus@yandex.ru>
2017-10-29 11:31:12 +0300

    mis weights implemented

--- vex/include/vl.h
+++ vex/include/vl.h
@@ -27,7 +27,12 @@ vector vl_old(int samples; float _maxdist, scatter, absorb)
 }
 
 
-
+float misweights(float pdf, otherpdf)
+{
+    return pdf/(pdf + otherpdf);
+    
+    //return pdf*pdf/(pdf*pdf + otherpdf*otherpdf);
+}
 
 
 vector vl(int samples; float _maxdist, scatter, absorb; vector perlight[])
@@ -37,6 +42,7 @@ vector vl(int samples; float _maxdist, scatter, absorb; vector perlight[])
     float ext = scatter + absorb;
     vector nI = normalize(I);
     maxdist = min(maxdist, length(I));
+    float pdf_u = 1/maxdist;
     vector tmp = 0; //accumulate all distributions here
 
     int lights[] = getlights();
@@ -59,19 +65,38 @@ vector vl(int samples; float _maxdist, scatter, absorb; vector perlight[])
         // loop over samples
         for (int sample=0; sample<samples; sample++)
         {   
+        
+            //EQUIANGULAR
             // compute stratified sample and pdf for it
-            float r = (rand(i+SID) + sample) /samples;
+            float r = (rand(i+sample+SID) + sample) /samples;
             float tt = D*tan( (1-r)*thetaA + r*thetaB );
-            float pdf = D/((thetaB-thetaA)*(D*D + tt*tt));
-            float dist = (delta + tt);
-            vector pos = nI * dist;
+            float pdf_e = D/((thetaB-thetaA)*(D*D + tt*tt));
+            float dist_e = (delta + tt);
+            vector pos_e = nI * dist_e;
+            vector tmp_e = 0;
 
             // illuminance loop only for current light
-            illuminance(pos, {0,0,0}, "lightmask", getlightname( lights[i] ) )
+            illuminance(pos_e, {0,0,0}, "lightmask", getlightname( lights[i] ) )
             {
-                shadow(Cl, pos, L);
-                tmpl += Cl*scatter*exp((-dist-length(L)) * ext)*0.5/pdf;
+                shadow(Cl, pos_e, L);
+                tmp_e += Cl*scatter*exp((-dist_e-length(L)) * ext)*0.5/pdf_e;
             }
+            tmpl +=tmp_e * misweights(pdf_e, pdf_u);
+            
+            // UNIFORM
+            vector tmp_u = 0;
+            float dist_u = maxdist * r;
+            vector pos_u = nI * dist_u;
+            illuminance(pos_u, {0,0,0}, "lightmask", getlightname( lights[i] ) )
+            {
+                shadow(Cl, pos_u, L);
+                tmp_u += Cl*scatter*exp((-dist_u-length(L)) * ext)*0.5/pdf_u;
+            }
+            tt = delta - dist_u;
+            pdf_e = D/((thetaB-thetaA)*(D*D + tt*tt));
+            tmpl +=tmp_u * misweights(pdf_u, pdf_e);
+            
+            
         }
         
         tmpl /= samples;
